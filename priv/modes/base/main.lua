748b763d92af0e3393071e304c5a4a63d472d42b
Tilman M. Jaeschke <asciibeats@users.noreply.github.com>
2019-01-31 10:56:42 +0100

    moved callback to call and cast; added option to pipe data

--- priv/modes/base/main.lua
+++ priv/modes/base/main.lua
@@ -1,11 +1,20 @@
-function fail()
-  a[1] = 0
-end
-
-function callerror(message, level)
-  error(message, level)
-end
-
 function echo(...)
+  print(...)
   return ...
+end
+
+function call(name, ...)
+  return luaport.call[name](...)
+end
+
+function cast(name, ...)
+  luaport.cast[name](...)
+end
+
+function getstate()
+  return state, nil
+end
+
+function execute(name, ...)
+  return luaport[name](...)
 end
--- src/luaport.erl
+++ src/luaport.erl
@@ -1,21 +1,22 @@
 -module(luaport).
 
 -export([spawn/2]).
--export([spawn/3]).
 -export([respawn/1]).
 -export([despawn/1]).
 -export([call/2]).
 -export([call/3]).
 -export([call/4]).
-%-export([cast/2]).
-%-export([cast/3]).
+-export([call/5]).
+-export([call/6]).
+-export([cast/2]).
+-export([cast/3]).
+-export([cast/4]).
+-export([cast/5]).
 
 -define(TIMEOUT, 1000).
 
 spawn(Id, Path) ->
-  ?MODULE:spawn(Id, Path, undefined).
-spawn(Id, Path, Callback) ->
-  luaport_sup:spawn(Id, Path, Callback).
+  luaport_sup:spawn(Id, Path).
 
 respawn(Id) ->
   luaport_sup:respawn(Id).
@@ -24,13 +25,21 @@ despawn(Id) ->
   luaport_sup:despawn(Id).
 
 call(Pid, Name) ->
-  call(Pid, Name, [], ?TIMEOUT).
+  call(Pid, Name, []).
 call(Pid, Name, Args) ->
   call(Pid, Name, Args, ?TIMEOUT).
 call(Pid, Name, Args, Timeout) ->
-  luaport_server:call(Pid, Name, Args, Timeout).
+  call(Pid, Name, Args, Timeout, undefined).
+call(Pid, Name, Args, Timeout, Callback) ->
+  call(Pid, Name, Args, Timeout, Callback, []).
+call(Pid, Name, Args, Timeout, Callback, Pipe) ->
+  luaport_server:call(Pid, Name, Args, Timeout, Callback, Pipe).
 
-%cast(Pid, Name) ->
-% cast(Pid, Name, []).
-%cast(Pid, Name, Args) ->
-% luaport_server:cast(Pid, Name, Args).
+cast(Pid, Name) ->
+ cast(Pid, Name, []).
+cast(Pid, Name, Args) ->
+ cast(Pid, Name, Args, undefined).
+cast(Pid, Name, Args, Callback) ->
+ cast(Pid, Name, Args, Callback, []).
+cast(Pid, Name, Args, Callback, Pipe) ->
+ luaport_server:cast(Pid, Name, Args, Callback, Pipe).
